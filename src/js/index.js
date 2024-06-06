6a14b6029a6435931d56592d8955eb7ef396ab0a
komysovpv <komysovpv@gmail.com>
2024-06-06 17:07:57 +1000

    add smooth scroll links

--- src/js/index.js
+++ src/js/index.js
@@ -43,3 +43,22 @@ procedures.forEach(item => {
     name.addEventListener('click', handleClick);
 })
 
+document.querySelectorAll(".header__link").forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let href = this.getAttribute('href').substring(1);
+
+        const scrollTarget = document.getElementById(href);
+        const topOffset = 0;
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
+
+
